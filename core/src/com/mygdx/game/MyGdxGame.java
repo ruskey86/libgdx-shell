9f1434f77aaac3a38839e84ed32c9bc504b86ce5
natescode <nate@natescode.com>
2015-06-22 17:43:38 -0500

    Got libgdx shell working

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -6,6 +6,8 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import java.awt.*;
+
 public class MyGdxGame extends ApplicationAdapter {
     SpriteBatch batch;
     Texture img;
@@ -23,10 +25,10 @@ public class MyGdxGame extends ApplicationAdapter {
 
     @Override
     public void render() {
-        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
-        batch.draw(img, 0, 0);
+        batch.draw(img, Gdx.graphics.getWidth() / 2 - img.getWidth() / 2, Gdx.graphics.getHeight() / 2 - img.getHeight() / 2);
         batch.end();
     }
 
@@ -37,7 +39,6 @@ public class MyGdxGame extends ApplicationAdapter {
 
     @Override
     public void resume() {
-
     }
 
     @Override
